413d2523a47ca594e311b049dddbd1734ce15437
raegonzle <regl.850221@gmail.com>
2025-10-05 19:54:47 -0500

    Normalize: ignorar .vs/bin/obj y limpiar índice

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d916f579707155c32cebc8b6e6bb202554d240c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ff1b7c552a363c62689078febf42ba15903ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
